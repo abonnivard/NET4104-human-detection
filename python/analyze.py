b1b0807c5835a83f958f7063fcab17d5d7d14060
Vvlili <149427796+Vvlili@users.noreply.github.com>
2024-04-24 09:37:39 +0200

    Adding the detection of "nothing"
    SOmething doesn't work when using the IA on data it never "saw" theorycally

--- python/analyze.py
+++ python/analyze.py
@@ -1,24 +1,26 @@
-cd eimport numpy as np
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import warnings
 import ast
 warnings.filterwarnings('ignore')
-from gen_dataset import d, d2
+from gen_dataset import d, d2, d3
 
 from sklearn.model_selection import train_test_split
 
-X = pd.concat([d, d2])['data'].apply(ast.literal_eval)
-X2 = np.array([np.array(x) for x in X])
+X = pd.concat([d, d2, d3])['data'].apply(ast.literal_eval)
+X2 = np.array([np.array(x) for x in X[1:]])
 X2 = np.vstack(X2)
-y = pd.concat([d, d2])['State']
+print(X2)
+X3 = pd.concat([d, d2, d3])['data']
+y = pd.concat([d, d2, d3])['State'][1:]
 
 
 X_train, X_test, y_train, y_test = train_test_split(X2, y, train_size=0.7, random_state=0)
 
 from sklearn import tree
-clf = tree.DecisionTreeClassifier()
+clf = tree.DecisionTreeClassifier(max_depth=13)
 clf.fit(X_train, y_train)
 
 
@@ -26,7 +28,7 @@ from sklearn.metrics import accuracy_score
 
 # Visualize the tree after training
 plt.figure(figsize=(10,8)) 
-tree.plot_tree(clf, class_names=['1', '0'], filled=True)
+tree.plot_tree(clf, class_names=['1', '0', '-1'], filled=True)
 plt.show()
 
 # Predict Accuracy Score on test dataset
@@ -34,3 +36,11 @@ y_pred = clf.predict(X_test)
 
 print("Train data accuracy:",round(100*accuracy_score(y_true = y_train, y_pred=clf.predict(X_train)),3),"%")
 print("Test data accuracy:","%.3f" %(100*accuracy_score(y_true = y_test, y_pred=y_pred)),"%")
+
+
+data = pd.read_csv("enregistrement/debout_14.csv")
+data_nv = data['data'].apply(ast.literal_eval)
+data_nv = np.array([np.array(x) for x in data_nv])
+data_nv = np.vstack(data_nv)
+plt.plot((data)['seq'].tolist(), clf.predict(data_nv))
+plt.show()
--- python/gen_dataset.py
+++ python/gen_dataset.py
@@ -2,17 +2,24 @@ import pandas as pd
 import warnings
 warnings.filterwarnings('ignore')
 
+
 d = pd.read_csv("enregistrement/allonge_0.csv")
 d2 = pd.read_csv("enregistrement/debout_0.csv")
+d3 = pd.read_csv("enregistrement/rien_1.csv")
 
-for i in range(1, 12):
+for i in range(1, 11):
     bis = pd.read_csv("enregistrement/allonge_" + str(i) + ".csv")
     pd.concat([d, bis])
     
-for i in range(1, 15) : 
+for i in range(1, 14) : 
     bis = pd.read_csv("enregistrement/debout_" + str(i) + ".csv")
     pd.concat([d2, bis])
+
+for i in range(2, 6) : 
+    bis = pd.read_csv("enregistrement/rien_" + str(i) + ".csv")
+    pd.concat([d3, bis])
 
 
 d['State'] = 0
 d2['State'] = 1
+d3['State'] = -1
