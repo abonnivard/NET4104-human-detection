81882128b0c3878c216663c8646476813f31c767
Vvlili <149427796+Vvlili@users.noreply.github.com>
2024-04-30 11:22:38 +0200

    IA final 80% reussite en moyenne

--- python/analyze.py
+++ python/analyze.py
@@ -5,42 +5,57 @@ import seaborn as sns
 import warnings
 import ast
 warnings.filterwarnings('ignore')
-from gen_dataset import d, d2, d3
 
+from sklearn.metrics import accuracy_score
+from sklearn import tree
 from sklearn.model_selection import train_test_split
 
+from gen_dataset import d, d2, d3
+
+
+#isolation du vecteur "data" de taille 109
 X = pd.concat([d, d2, d3])['data'].apply(ast.literal_eval)
-X2 = np.array([np.array(x) for x in X[1:]])
-X2 = np.vstack(X2)
-print(X2)
-X3 = pd.concat([d, d2, d3])['data']
-y = pd.concat([d, d2, d3])['State'][1:]
+X_data = np.array([np.array(x) for x in X])
+X_data = np.vstack(X_data)
 
 
-X_train, X_test, y_train, y_test = train_test_split(X2, y, train_size=0.7, random_state=0)
+#version avec le rssi
+X_rssi = pd.concat([d, d2, d3])[['rssi']]
 
-from sklearn import tree
-clf = tree.DecisionTreeClassifier(max_depth=13)
+#creation des étiquettes
+y = pd.concat([d, d2, d3])['State']
+
+
+#split des données de tests, 70% pour l'entrainement, 30% pour les tests
+X_train, X_test, y_train, y_test = train_test_split(X_data, y, train_size=0.7, random_state=0)
+
+#on force l'arbre à n'avoie qu'une profondeur de 15 pour éviter que l'IA apprenne le jeu par coeur
+clf = tree.DecisionTreeClassifier(criterion='entropy', max_depth=15)
 clf.fit(X_train, y_train)
 
 
-from sklearn.metrics import accuracy_score
-
-# Visualize the tree after training
-plt.figure(figsize=(10,8)) 
+# Afficher l'arbre
+plt.figure(figsize=(10,10)) 
 tree.plot_tree(clf, class_names=['1', '0', '-1'], filled=True)
 plt.show()
 
-# Predict Accuracy Score on test dataset
+# prédiction test et affichage du score de réussite
 y_pred = clf.predict(X_test)
 
 print("Train data accuracy:",round(100*accuracy_score(y_true = y_train, y_pred=clf.predict(X_train)),3),"%")
 print("Test data accuracy:","%.3f" %(100*accuracy_score(y_true = y_test, y_pred=y_pred)),"%")
 
 
-data = pd.read_csv("enregistrement/debout_14.csv")
+#test avec une donnée ne faisant pas partie du jeu de test
+data = pd.read_csv("enregistrement/allonge_5.csv")
+data['State'] = 0
+
+data_nv_2 = data[['rssi']]
+
 data_nv = data['data'].apply(ast.literal_eval)
 data_nv = np.array([np.array(x) for x in data_nv])
 data_nv = np.vstack(data_nv)
+
+print("Test data accuracy:","%.3f" %(100*accuracy_score(y_true = data['State'], y_pred=clf.predict(data_nv))),"%")
 plt.plot((data)['seq'].tolist(), clf.predict(data_nv))
 plt.show()
--- python/gen_dataset.py
+++ python/gen_dataset.py
@@ -3,21 +3,22 @@ import warnings
 warnings.filterwarnings('ignore')
 
 
-d = pd.read_csv("enregistrement/allonge_0.csv")
-d2 = pd.read_csv("enregistrement/debout_0.csv")
-d3 = pd.read_csv("enregistrement/rien_1.csv")
+d = pd.read_csv("enregistrement/allonge_2.csv")
+d2 = pd.read_csv("new_enregistrements/debout/debout_1.csv")
+d3 = pd.read_csv("new_enregistrements/rien/rien_1.csv")
 
-for i in range(1, 11):
+for i in range(2, 3):
     bis = pd.read_csv("enregistrement/allonge_" + str(i) + ".csv")
     pd.concat([d, bis])
     
-for i in range(1, 14) : 
-    bis = pd.read_csv("enregistrement/debout_" + str(i) + ".csv")
-    pd.concat([d2, bis])
+for i in range(1, 4) : 
+    if i != 2 :
+        bis = pd.read_csv("new_enregistrements/debout/debout_" + str(i) + ".csv")
+        pd.concat([d2, bis])
 
-for i in range(2, 6) : 
-    bis = pd.read_csv("enregistrement/rien_" + str(i) + ".csv")
-    pd.concat([d3, bis])
+for i in range(1, 4) : 
+     bis = pd.read_csv("new_enregistrements/rien/rien_" + str(i) + ".csv")
+     pd.concat([d3, bis])
 
 
 d['State'] = 0
